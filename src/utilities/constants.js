1412fddea7acb6da8d45956e05acbcbd66420a1a
Thieu Dinh Nam <thieudinhnam@Thieus-MacBook-Air.local>
2023-01-16 13:21:50 +0700

    update

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,5 +1,5 @@
-// export const API_ROOT = 'https://trello-namdeft-api.vercel.app'
-export const API_ROOT = 'http://localhost:8080'
+export const API_ROOT = 'https://trello-namdeft-api.vercel.app'
+// export const API_ROOT = 'http://localhost:8080'
 
 export const MODAL_ACTION_CLOSE = 'MODAL_ACTION_CLOSE'
 export const MODAL_ACTION_CONFIRM = 'MODAL_ACTION_CONFIRM'
